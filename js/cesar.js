1faa10fddbf072bf47118ba93cb5d9f32a1331b3
Alan Cordoba <alancordoba010@gmail.com>
2023-06-17 18:04:59 -0300

    Update

--- js/cesar.js
+++ js/cesar.js
@@ -1,49 +1,62 @@
 window.addEventListener("load", inicio);
 
 function inicio() {
-  document.querySelector("#cifrar").addEventListener("click", () => {
-    const texto = document.getElementById("texto").value;
-    const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
-    document.getElementById("resultado").innerHTML = cifrar(texto, desplazamiento);
-  });
+  const cifrarButton = document.querySelector("#cifrar");
+  const descifrarButton = document.querySelector("#descifrar");
 
-  document.querySelector("#descifrar").addEventListener("click", () => {
-    const texto = document.getElementById("texto").value;
-    const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
-    document.getElementById("resultado").innerHTML = descifrar(texto, desplazamiento);
-  });
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+
+  if (isTouchDevice) {
+    cifrarButton.addEventListener("touchend", handleCifrar);
+    descifrarButton.addEventListener("touchend", handleDescifrar);
+  } else {
+    cifrarButton.addEventListener("click", handleCifrar);
+    descifrarButton.addEventListener("click", handleDescifrar);
+  }
+}
+
+function handleCifrar() {
+  const texto = document.getElementById("texto").value;
+  const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
+  document.getElementById("resultado").innerHTML = cifrar(texto, desplazamiento);
+}
+
+function handleDescifrar() {
+  const texto = document.getElementById("texto").value;
+  const desplazamiento = parseInt(document.getElementById("desplazamiento").value);
+  document.getElementById("resultado").innerHTML = descifrar(texto, desplazamiento);
 }
 
 function cifrar(text, desplazamiento) {
-  if (!text) return "";
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  desplazamiento = ((desplazamiento % 26) + 26) % 26;
-
-  let resultado = "";
-  for (let i = 0; i < text.length; i++) {
-    const c = text[i];
-    const posicion = letras.indexOf(c);
-    const nuevaPosicion = (posicion !== -1) ? (posicion + desplazamiento) % 26 : -1;
-    const nuevaLetra = (nuevaPosicion !== -1) ? letras[nuevaPosicion] : c;
-    resultado += (c >= 'a') ? nuevaLetra.toLowerCase() : nuevaLetra;
+    if (!text) return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    desplazamiento = ((desplazamiento % 26) + 26) % 26;
+  
+    let resultado = "";
+    for (let i = 0; i < text.length; i++) {
+      const c = text[i];
+      const posicion = letras.indexOf(c);
+      const nuevaPosicion = (posicion !== -1) ? (posicion + desplazamiento) % 26 : -1;
+      const nuevaLetra = (nuevaPosicion !== -1) ? letras[nuevaPosicion] : c;
+      resultado += (c >= 'a') ? nuevaLetra.toLowerCase() : nuevaLetra;
+    }
+  
+    return resultado;
   }
-
-  return resultado;
-}
-
-function descifrar(text, desplazamiento) {
-  if (!text) return "";
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  desplazamiento = ((desplazamiento % 26) + 26) % 26;
-
-  let resultado = "";
-  for (let i = 0; i < text.length; i++) {
-    const c = text[i];
-    const posicion = letras.indexOf(c);
-    const nuevaPosicion = (posicion !== -1) ? (posicion - desplazamiento) % 26 : -1;
-    const nuevaLetra = (nuevaPosicion !== -1) ? letras[nuevaPosicion] : c;
-    resultado += (c >= 'a') ? nuevaLetra.toLowerCase() : nuevaLetra;
+  
+  function descifrar(text, desplazamiento) {
+    if (!text) return "";
+    const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    desplazamiento = ((desplazamiento % 26) + 26) % 26;
+  
+    let resultado = "";
+    for (let i = 0; i < text.length; i++) {
+      const c = text[i];
+      const posicion = letras.indexOf(c);
+      const nuevaPosicion = (posicion !== -1) ? (posicion - desplazamiento) % 26 : -1;
+      const nuevaLetra = (nuevaPosicion !== -1) ? letras[nuevaPosicion] : c;
+      resultado += (c >= 'a') ? nuevaLetra.toLowerCase() : nuevaLetra;
+    }
+  
+    return resultado;
   }
-
-  return resultado;
-}
